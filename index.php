88501410231651c641f514377b1a558a2e439ddd
Akuma_Yu <manjusaka1110@gmail.com>
2025-06-04 15:40:03 +0800

    針對增加 刪除 的項目功能修改index.php

--- index.php
+++ index.php
@@ -20,8 +20,23 @@
             border: 1px solid black;
             text-align: center;
         }
+
         a {
             text-decoration: none;
+        }
+
+        .container {
+            width: 500px;
+            height: 30px;
+            display: flex;
+            justify-content: start;
+            align-items: center;
+            margin:auto;
+            /* background-color: green; */
+        }
+
+        .report {
+            margin-left: 10px;
         }
     </style>
 </head>
@@ -29,12 +44,18 @@
 <body>
     <h1>泰山泰好吃早餐店</h1>
     <h2>餐點項目</h2>
+
     <?php
     $dsn = "mysql:host=localhost;dbname=store;charset=utf8";
     $pdo = new PDO($dsn, 'root', '');
     $items = $pdo->query("SELECT `id`, `name`, `price` FROM items")->fetchAll(PDO::FETCH_ASSOC);
     ?>
+    <div class="container">
+        <button><a href="./add_item.php">+</a></button>
+        <button class="report"><a href="./sales_report.php">銷售報表</a></button>
+    </div>
     <table>
+
         <tr>
             <td>品項</td>
             <td>價格</td>
@@ -46,7 +67,8 @@
                 <td><?= $item['name'] ?></td>
                 <td>
                     <?= $item['price'] ?>
-                        <a href='./Edit_item_price.php?id=<?=$item['id'];?>'>Edit</a>
+                    <a href='./Edit_item_price.php?id=<?= $item['id']; ?>'>Edit</a>
+                    <a href='./api/del_item.php?id=<?= $item['id']; ?>'>Del</a>
                 </td>
             </tr>
         <?php
